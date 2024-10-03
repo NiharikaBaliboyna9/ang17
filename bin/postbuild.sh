cf30bde8edeb9e107332208918b356262306383c
Niharika <balibon@amazon.com>
2024-10-03 23:59:19 +0530

    first push 999

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -18,8 +18,8 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./dist/angular-new/server ./.amplify-hosting/compute/default
+cp -r ./dist/angluar-new/server ./.amplify-hosting/compute/default
 
-cp -r ./dist/angular-new/browser ./.amplify-hosting/static
+cp -r ./dist/angluar-new/browser ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
